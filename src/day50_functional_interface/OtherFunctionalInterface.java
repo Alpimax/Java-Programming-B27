5c213e6253a14277b0982f25c85d52a56d269560
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-10-04 12:02:35 -0500

    day 50 functional interface

--- src/day50_functional_interface/OtherFunctionalInterface.java
+++ src/day50_functional_interface/OtherFunctionalInterface.java
@@ -2,9 +2,9 @@ package day50_functional_interface;
 
 import day47_collections.Person;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 
 public class OtherFunctionalInterface {
@@ -40,8 +40,31 @@ public class OtherFunctionalInterface {
 
         classRoom.forEach((key, value) -> System.out.println("Key: " + key + " Value: " + value));
 
+        BiFunction<Integer, Integer, Integer> maxNumber = (a, b) -> a > b ? a : b;
+        //  (a, b) -> { return a > b ? a : b;};
+        System.out.println(maxNumber.apply(6, 12));
 
+        // make a map of many data -> keys -> set , value -> list
 
+        /*
+        Set<String> - parameter 1
+        List<Integer> - parameter 2
+         Map<String, Integer> - return type
+         */
+        BiFunction<Set<String>, List<Integer>, Map<String, Integer>> createMap = (emails, inboxNumbers) -> {
+            List<String> listEmails = new ArrayList<>(emails);
+            Map<String, Integer> map = new HashMap<>();
+
+            for(int i = 0; i < listEmails.size(); i++){
+                map.put(listEmails.get(i), inboxNumbers.get(i));
+            }
+            return map;
+        };
+
+        Set<String> emails = new LinkedHashSet<>(Arrays.asList("james@007.com", "lionlover@gmail.com", "macComputers@gmail.com", "javaisfun@gmail.com"));
+        List<Integer> inboxNumbers = new ArrayList<>(Arrays.asList(300, 150, 200, 1000));
+
+        System.out.println(createMap.apply(emails, inboxNumbers));
 
     }
 }
