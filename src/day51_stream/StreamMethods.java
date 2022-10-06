86bc04b64326b7941ced214b78ba5f30684d778b
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-10-06 11:50:53 -0500

    Day 51 - Bye world

--- src/day51_stream/StreamMethods.java
+++ src/day51_stream/StreamMethods.java
@@ -33,9 +33,36 @@ public class StreamMethods {
         arr2 = Arrays.stream(arr2).distinct().toArray();
         System.out.println("distinct w/ array: " + Arrays.toString(arr2));
 
+        System.out.println("---------------------------------------");
 
+        // skip(n): skips/removes the n number of elements
+        List<String> list3 = new ArrayList<>(Arrays.asList("summer", "bike", "fall", "raining", "pond", "lake", "fishing", "sun"));
+        System.out.println(list3.stream().skip(3).collect(Collectors.toList()));
+        System.out.println(list3);
+        System.out.println(list3.stream().skip(6).collect(Collectors.toList()));
+        System.out.println(list3.stream().skip(10).collect(Collectors.toList()));
 
+        System.out.println("---------------------------------------");
 
+        // limit(n): keep a n number of elements
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(list4.stream().limit(5).collect(Collectors.toList()));
+
+        // using both limit & skip
+        System.out.println(list4.stream().limit(6).skip(2).collect(Collectors.toList()));
+        System.out.println(list4.stream().skip(4).limit(4).collect(Collectors.toList()));
+
+        System.out.println("---------------------------------------");
+
+        // count: how many elements/data are in the stream
+        List<Integer> list5 = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 1, 2, 3, 4, 5, 6, 6, 7, 6, 5, 4, 2, 4, 1, 5, 7, 4, 2));
+        System.out.println(list5.stream().count());
+        System.out.println(list5.stream().distinct().count());
+
+        // task: given a String determine the number of unique characters
+        String s = "aaaaabbbbbbcccccddddeeeeeeffff";
+        System.out.println(Arrays.stream(s.split("")).distinct().count());
+        System.out.println(Arrays.stream(s.split("")).distinct().collect(Collectors.toList())); // you could have also converted the stream to an array: toArray() instead of Collect to list
 
     }
 }
